815216be4e05f060d0b8d925decbf2700cba1e1a
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-05-16 20:55:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function setNavbarInnerHTML() {
-    document.querySelector("navbar").innerHTML = `
+    document.querySelector(".navbar").innerHTML = `
         <a href="page1.html">Page 1</a>
         <a href="page2.html">Page 2</a>
     `;
