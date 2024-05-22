2bdcbd60da6cb9ceac833ca5bf42aa06ed5597f8
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-05-21 20:28:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function setNavbarInnerHTML() {
     document.querySelector(".navbar").innerHTML = `
         <a href="./">Home Page</a>
         <a href="firstammendment.html">First Amendment Freedoms
-</a>        <a href="firstammendment.html">Civics Education
-</a>        <a href="firstammendment.html">Benefits of Civics Education
-</a>        <a href="firstammendment.html">What We Could Buy
-</a>        <a href="firstammendment.html">Civics and the Public Good
+</a>        <a href="civicsedu.html">Civics Education
+</a>        <a href="benefits.html">Benefits of Civics Education
+</a>        <a href="buy.html">What We Could Buy
+</a>        <a href="publicgood.html">Civics and the Public Good
 </a>
     `;
 }
