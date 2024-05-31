0306942178f9c95d584c5d55e54f79730846b492
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-05-31 19:05:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ for(var i = 0; i < img.length; i++){
 	img[i].outerHTML = "<center>" + img[i].outerHTML + "</center>";
 }
 if(document.querySelector("h1").innerHTML.startsWith("Civics Education Advocacy") == false){
-	document.querySelector("h1").innerText = "Civics Education Advocacy" + document.querySelector("h1").innerText;
+	document.querySelector("h1").innerText = "Civics Education Advocacy - " + document.querySelector("h1").innerText;
 }
 }
 document.addEventListener("DOMContentLoaded", whenLoaded);
